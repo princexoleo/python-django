3eaefa8867e838b0a72a581e36e87aecf1e49014
maz_leo <xtleon.xl@gmail.com>
2019-03-22 03:04:55 +0600

    js:basics

---
+++ javascript/level_0ne/excercise.js
@@ -0,0 +1,4 @@
+var lbs = prompt("Weight in lbs>?");
+var kg = lbs*0.454;
+alert("This is : "+kg+" kilograms");
+console.log("Conversion completed!");
---
+++ javascript/level_0ne/myscript.js
@@ -0,0 +1,4 @@
+alert("Welcome to Your Bank");
+var deposit = prompt("How much you like to deposit today? ");
+alert("You've deposit: "+deposit);
+console.log("You are a cool person");
